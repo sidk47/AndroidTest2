395c0f41c5f0b760fa2f1a93986b7e2640b3dee1
Siddharth Karunakaran <sidk47@gmail.com>
2018-04-22 12:24:58 +0530

    Added a JOB

--- app/src/main/java/com/siddharth/androidtest2/MainActivity.kt
+++ app/src/main/java/com/siddharth/androidtest2/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var occupation = "Developer"
     }
 
     //This is the button added branch
